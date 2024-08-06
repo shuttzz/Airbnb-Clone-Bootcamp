7404f3260479013964d5847100c384900b532dfe
José Assis <netomenezesucg@gmail.com>
2024-08-05 22:03:02 -0300

    structure  initial for viewCode

---
+++ AirbnbClone/AirbnbClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirbnbClone
+//
+//  Created by José Assis de Menezes Neto on 05/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
